67eaf39f6a2bc9ac47496c9f3dfa4b59bd208efb
ybnd <ybnd@tuta.io>
2020-02-16 14:47:16 +0100

    Add .ploy file

--- deploy.py
+++ deploy.py
@@ -1,15 +1,19 @@
 import os
 from dulwich import porcelain
+import yaml
+
 
 cwd = os.getcwd()
-url = "https://github.com/ybnd/gitploy"
-branch = 'master'
+
+if os.path.isfile('.ploy'):
+    with open('.ploy', 'r') as f:
+        ploy = yaml.safe_load(f.read())
 
 if os.path.isdir('.git'):
     print(f"Opening repository in {cwd}")
     repo = porcelain.open_repo(cwd)
 else:
-    print(f"Cloning remote repository from {url} to {cwd}")
-    repo = porcelain.clone(url ,cwd)
+    print(f"Cloning remote repository from {ploy['url']} to {cwd}")
+    repo = porcelain.clone(ploy['url'] ,cwd)
 
-fetched_data = porcelain.fetch(repo, url)
+fetched_data = porcelain.fetch(repo, ploy['url'])
