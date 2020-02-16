e13f9d42b0ea857ca456609ef922310b858138d8
ybnd <ybnd@tuta.io>
2020-02-16 15:35:26 +0100

    Clean up deploy.py

--- deploy.py
+++ deploy.py
@@ -5,6 +5,30 @@ import re
 import subprocess
 
 
+gitploy = '''# Bootstrap .git connection with dulwich
+
+import os
+from dulwich import porcelain
+import yaml
+
+
+cwd = os.getcwd()
+
+if os.path.isfile('.ploy'):
+    with open('.ploy', 'r') as f:
+        ploy = yaml.safe_load(f.read())
+
+if os.path.isdir('.git'):
+    print(f"Opening repository in {cwd}")
+    repo = porcelain.open_repo(cwd)
+else:
+    print(f"Cloning remote repository from {ploy['url']} to {cwd}")
+    repo = porcelain.clone(ploy['url'] ,cwd)
+
+fetched_data = porcelain.fetch(repo, ploy['url'])
+'''
+
+
 # Change working directory if called from scripts/
 og_cwd = os.getcwd()
 if og_cwd.split('/')[-1] == 'scripts':
@@ -34,28 +58,7 @@ subprocess.check_call([environment + '/bin/python', '-m', 'pip', 'install', '-r'
 
 # Deposit temporary script to set up .git
 with open('gitploy.py', 'w') as f:
-    f.write(
-    '''import os
-from dulwich import porcelain
-import yaml
-
-
-cwd = os.getcwd()
-
-if os.path.isfile('.ploy'):
-    with open('.ploy', 'r') as f:
-        ploy = yaml.safe_load(f.read())
-
-if os.path.isdir('.git'):
-    print(f"Opening repository in {cwd}")
-    repo = porcelain.open_repo(cwd)
-else:
-    print(f"Cloning remote repository from {ploy['url']} to {cwd}")
-    repo = porcelain.clone(ploy['url'] ,cwd)
-
-fetched_data = porcelain.fetch(repo, ploy['url'])
-'''
-    )
+    f.write(gitploy)
 
 # Set up .git
 subprocess.check_call([environment + '/bin/python', 'gitploy.py'])
