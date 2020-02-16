07645f7e08bb80949efdc6f6f5452323e9fc1652
ybnd <ybnd@tuta.io>
2020-02-16 15:03:39 +0100

    Update scripts; bootstrap virtual environment

--- deploy.py
+++ deploy.py
@@ -1,19 +1,32 @@
+# Bootstrap a virtual environment (see https://github.com/ybnd/bootstrap-venv)
+
 import os
-from dulwich import porcelain
-import yaml
+import re
+import subprocess
 
 
-cwd = os.getcwd()
+# Get environment name
+environment = None
 
 if os.path.isfile('.ploy'):
+    # Read environment name from .ploy (without PyYAML)
     with open('.ploy', 'r') as f:
-        ploy = yaml.safe_load(f.read())
+        yaml = f.read()
+        
+    match = re.search('environment:[\s]*(.*)[\r\n]*', yaml)
+    environment = match.group(1)
 
-if os.path.isdir('.git'):
-    print(f"Opening repository in {cwd}")
-    repo = porcelain.open_repo(cwd)
-else:
-    print(f"Cloning remote repository from {ploy['url']} to {cwd}")
-    repo = porcelain.clone(ploy['url'] ,cwd)
+if environment is None:
+    environment = '.venv'
 
-fetched_data = porcelain.fetch(repo, ploy['url'])
+
+# Create a virtual environment in .venv
+subprocess.check_call(['python', '-m', 'venv', environment])
+
+# Install requirements.txt
+subprocess.check_call([environment + '/bin/python', '-m', 'pip', 'install', '--upgrade', 'pip'])
+subprocess.check_call([environment + '/bin/python', '-m', 'pip', 'install', '-r', 'requirements.txt'])
+
+# Run gitploy.py with 
+subprocess.check_call([environment + '/bin/python', 'gitploy.py'])
+
--- undeploy.py
+++ undeploy.py
@@ -1,8 +1,27 @@
 import os
 import shutil
 
-if os.path.isfile('README.md'):
-    os.remove('README.md')
 
+# Get environment name
+environment = None
+
+if os.path.isfile('.ploy'):
+    # Read environment name from .ploy (without PyYAML)
+    with open('.ploy', 'r') as f:
+        yaml = f.read()
+        
+    match = re.search('environment:[\s]*(.*)[\r\n]*', yaml)
+    environment = match.group(1)
+
+if environment is None:
+    environment = '.venv'
+    
+    
+# Remove virtual environment
+if os.path.isdir('.venv'):
+    shutil.rmtree('.venv')
+
+# Remove .git
 if os.path.isdir('.git'):
     shutil.rmtree('.git')
+
